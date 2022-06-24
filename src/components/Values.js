ff882fd2774ba1c364dd64a00e433a0bdd28b962
veysel basbaydar <vgbasbaydar@gmail.com>
2022-06-23 21:45:20 -0700

    assesment is done

--- src/components/Values.js
+++ src/components/Values.js
@@ -8,28 +8,36 @@ const Values = () => {
       <br />
       <div>
         <div>
-          <h2>Think big and scale</h2>
+          <h2>
+            <u>Think big and scale</u>
+          </h2>
           <p>
             ​We're always looking for ways to simplify, do better, and go faster
             to impact over 2.2 billion women.
           </p>
         </div>
         <div>
-          <h2>Take ownership</h2>
+          <h2>
+            <u>Take ownership</u>
+          </h2>
           <p>
             You'll be given the autotomy and trust you need. It's okay to fail,
             learn, and try again - that's how we grow!
           </p>
         </div>
         <div>
-          <h2>Work hard, play hard</h2>
+          <h2>
+            <u>Work hard, play hard</u>
+          </h2>
           <p>
             Our team works hard to bring our mission to life, but that doesn't
             mean that we can't have fun while we're doing it!
           </p>
         </div>
         <div>
-          <h2>People and Health first</h2>
+          <h2>
+            <u>People and Health first</u>
+          </h2>
           <p>
             We take care of our team! We eat healthy, exercise together and
             offer premium healthcare.
